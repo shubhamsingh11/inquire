c0e506d13c4b100dddc8ed9f3964d3876fc2b887
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-10-24 10:24:18 +0530

    Tailwind config done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+import { Navbar } from './components/Navbar';
+import { Routes } from './components/Routes';
+import { Footer } from './components/Footer';
+
+const App = () => {
+    const [darkTheme , setDarkTheme] = useState(false);
+
+    return (
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-100 dark:bg-gray-100 dark:text-gray-100 min-h-screen">
+                <Navbar />
+                <Routes />
+                <Footer />
+            </div>
+        </div>
+    )
+}
+
+export default App;

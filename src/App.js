004c72691d8c5930c61cb20ce8df8b848a984468
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-10-24 10:54:24 +0530

    Implemented Dark/Light Mode

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
 
     return (
         <div className={darkTheme ? 'dark' : ''}>
-            <div className="bg-gray-100 dark:bg-gray-100 dark:text-gray-100 min-h-screen">
-                <Navbar />
+            <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
                 <Routes />
                 <Footer />
             </div>
